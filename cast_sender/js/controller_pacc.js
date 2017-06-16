ece8c2a0daeff207323f143879e4626c1e7cac98
Sämy Zehnder <saemy.zehnder@gorrion.ch>
2017-06-16 17:08:15 +0200

    Moved the pa_module to its own repository.

--- cast_sender/js/controller_pacc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var paccControllers = angular.module('paccControllers');
-
-paccControllers.controller('PACCCtrl', ['$scope', 'CCConnector', 'PAConnector',
-    function($scope, CCConnector, PAConnector) {
-        $scope.CCConnector = CCConnector;
-        $scope.PAConnector = PAConnector;
-    }]);
